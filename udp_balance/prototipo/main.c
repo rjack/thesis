3998bb1f61c28e1e20431ac880ff3192bb80d4ac
Giacomo Ritucci <giacomo.ritucci@email.it>
2008-11-16 22:33:30 +0100

    Inizio abbozzo strutture dati e funzioni

--- udp_balance/prototipo/main.c
+++ udp_balance/prototipo/main.c
@@ -4,26 +4,59 @@
 
 #include <assert.h>
 #include <errno.h>
+#include <linux/types.h>     /* workaround bug ubuntu: serve per errueue.h */
+#include <linux/errqueue.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 
 /****************************************************************************
-			     Definizioni di tipo
+			Definizioni di tipo e costanti
 ****************************************************************************/
 
 typedef int bool;
 
 #ifdef FALSE
-#undef FALSE
+#  undef FALSE
 #endif
 #define FALSE ((bool)0)
 
 #ifdef TRUE
-#undef TRUE
+#  undef TRUE
 #endif
 #define TRUE (!FALSE)
+
+
+/* Indici canali importanti */
+
+#define     SP_I                0         /* softphone */
+#define     IM_I                1         /* interface monitor */
+#define     CURRENT_IFACE_I     2
+
+
+/* XXX C'e' modo di scoprire a runtime la dimensione di allocazione per il
+ * XXX msg_control delle struct msghdr e poterlo cosi' allocare dinamicamente
+ * XXX senza rischiare un MSG_TRUNC quando si fa una recvmsg con flag MSG_ERRQUEUE?
+ * XXX Nell'attesa di scoprirlo uso la costante trovata nel sorgente di
+ * XXX traceroute <http://traceroute.sf.net/> */
+#define     CONTROLBUFLEN     1024
+
+
+struct chan {
+	/* socket file descriptor */
+	int ch_sfd;
+
+	/* indirizzi locale e remoto */
+	struct sockaddr_in ch_bind_addr;
+	struct sockaddr_in ch_remote_addr;
+
+	/* buffer per campo msg_control di struct msghdr */
+	char ch_controlbuf[CONTROLBUFLEN];
+};
 
 
 /****************************************************************************
@@ -32,13 +65,18 @@ typedef int bool;
 
 static const char *program_name = NULL;
 
+struct chan **channels;
+size_t channels_num = CURRENT_IFACE_I;
+
 
 /****************************************************************************
 			  Prototipi funzioni locali
 ****************************************************************************/
 
+static void *my_alloc (size_t nbytes);
 static void print_usage (void);
 static bool is_done (void);
+static void collect_garbage (void);
 
 
 /****************************************************************************
@@ -72,16 +110,35 @@ main (const int argc, const char *argv[])
 	 *
 	 * dati netconf -> fix socket
 	 *
-	 * tmout 150ms -> pulizia dati vecchi outward
+	 * tmout qualita' conversazione (150ms) -> pulizia dati vecchi outward
 	 *
 	 * scrivo a PX -> reset tmout keepalive
 	 *             -> datagram nel limbo, con associato tmout 30ms
+	 *
+	 * tmout limbo -> pacchetto scaduto in outward
+	 *                (XXX in coda o in testa?)
 	 */
 
 	if (argc != 1) {
 		print_usage ();
 		exit (EXIT_FAILURE);
 	}
+
+	/*
+	 * Setup iniziale.
+	 */
+
+	/*
+	 * Allocazione canali per:
+	 * - softphone
+	 * - trasmission error detector
+	 * - interface monitor
+	 */
+	assert (channels_num == 2);
+	channels = (struct chan **)my_alloc (channels_num * sizeof(struct chan *));
+
+	channels[SP_I] = new_chan ("127.0.0.1", 7777, "127.0.0.1", 8888);
+	channels[IM_I] = new_chan ("127.0.0.1", 5656, NULL, -1);
 
 	while (!is_done ()) {
 	}
@@ -93,6 +150,34 @@ main (const int argc, const char *argv[])
 /****************************************************************************
 			       Funzioni locali
 ****************************************************************************/
+
+static void
+collect_garbage (void)
+{
+	;
+}
+
+
+static void *
+my_alloc (size_t nbytes)
+{
+	void *new;
+
+	assert (nbytes > 0);
+
+	new = malloc (nbytes);
+	if (new == NULL) {
+		collect_garbage ();
+		new = malloc (nbytes);
+		if (new == NULL) {
+			perror ("Errore allocazione");
+			exit (EXIT_FAILURE);
+		}
+	}
+
+	return new;
+}
+
 
 static void
 print_usage (void)
