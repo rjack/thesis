58b64222bc252a5c6c4396ac04529db0c5238af6
Giacomo Ritucci <giacomo.ritucci@email.it>
2008-11-26 15:06:15 +0100

    .gitignore, fix nome eseguibile

--- udp_balance/prototipo/config.mk
+++ udp_balance/prototipo/config.mk
@@ -4,7 +4,7 @@ CPPOPT=-Wall -Wno-unused -ansi -pedantic
 CFLAGS=-g -pipe
 LDFLAGS=-lm -lc
 
-EXE=ulp-proto
+EXE=ulb-proto
 OBJ=crono.o dgram.o iface.o main.o util.o list.o
 
 DEPCOM=Makefile types.h
