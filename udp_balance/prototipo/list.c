0c7cd1d6fffc85fda2bfb4c13d93aaf2b900e4e7
Giacomo Ritucci <giacomo.ritucci@email.it>
2008-12-09 00:38:04 +0100

    Refactoring liste

--- udp_balance/prototipo/list.c
+++ udp_balance/prototipo/list.c
@@ -3,89 +3,181 @@
 #include <unistd.h>
 
 #include "list.h"
+#include "types.h"
+
 
 struct list_info {
-	list_node_t *ls_tail_ptr;
-	void (*ls_node_value_destroy)(void *);
-	size_t ls_node_value_size;
+	list_node_t *li_tail_ptr;
+	void (*li_node_value_destroy)(void *);
+	size_t li_node_value_size;
+	int li_list_len;
 };
 
 
-static struct list_info *list_db;
-static size_t list_db_size;
-static size_t list_db_used;
+static struct list_info *db;
+static size_t db_size;
+static size_t db_used;
+
+
+#ifndef NDEBUG
+static bool
+module_ok (void)
+{
+	bool ok = TRUE;
+
+	if (db_used < 0)
+		ok = FALSE;
+	if (db_size < 0)
+		ok = FALSE;
+
+	if (db == NULL) {
+		if (db_size != 0)
+			ok = FALSE;
+		if (db_used != 0)
+			ok = FALSE;
+	} else {
+		if (db_size == 0)
+			ok = FALSE;
+		if (db_used > db_size)
+			ok = FALSE;
+	}
+
+	return ok;
+}
+endif /* NDEBUG */
 
 
 list_t
 list_create (void (*node_value_destroy)(void *), size_t node_value_size)
+/*
+ * Crea una nuova lista e ritorna il suo handler, oppure LIST_ERR se fallisce.
+ * node_value_destroy e' la funzione per deallocare il valore degli elementi
+ * puntati dai nodi della lista, node_value_size e' la dimensione di ogni
+ * elemento.
+ */
 {
 	list_t new_handle;
 	struct list_info *new_list_info;
 
-	if ((list_db_size - list_db_used) == 0) {
-		struct list_info *new_list_db;
-		new_list_db = realloc (list_db, (list_db_size + 1)
-					     * sizeof(struct list_info));
-		if (new_list_db == NULL)
+	assert (module_ok ());
+
+	if ((db_size - db_used) == 0) {
+		struct list_info *new_db;
+		new_db = realloc (db, (db_size + 1)
+						 * sizeof(struct list_info));
+		if (new_db == NULL)
 			return LIST_ERR;
-		list_db = new_list_db;
-		list_db_size++;
+		db = new_db;
+		db_size++;
 	}
 
-	new_handle = list_db_used;
-	list_db_used++;
-	new_list_info = &(list_db[new_handle]);
-	new_list_info->ls_tail_ptr = NULL;
-	new_list_info->ls_node_value_destroy = node_value_destroy;
-	new_list_info->ls_node_value_size = node_value_size;
+	new_handle = db_used;
+	db_used++;
+
+	new_list_info = &(db[new_handle]);
+	new_list_info->li_tail_ptr = NULL;
+	new_list_info->li_node_value_destroy = node_value_destroy;
+	new_list_info->li_node_value_size = node_value_size;
+	new_list_info->li_list_len = 0;
 
 	return new_handle;
 }
 
 
 void
-list_destroy (list_t ls)
+list_destroy (list_t lst)
+/* 
+ * Libera la memoria associata ad ogni oggetto contenuto nella lista indicata
+ * e alla lista stessa.
+ * XXX non rialloca db, per quello c'e' list_garbage_collect.
+ */
 {
-	list_node_t *node;
-	void *freefun (void *);
+	void *element;
+	void (*my_free)(void *);
 
-	while ((node = list_dequeue (tp)) != NULL) {
-		freefun (node->n_ptr);
-		free (node);
+	assert (module_ok ());
+	assert (list_is_valid (lst));
+
+	/* Svuotamento lista */
+	my_free = db[lst].li_node_value_destroy;
+	while ((element = list_dequeue (lst)) != NULL)
+		my_free (element);
+
+	/* Rimozione da db. */
+	db_used--;
+	if (db_used != 0)
+		memcpy (&(db[lst]), &(db[db_used]),
+		        sizeof(*db));
+}
+
+
+void
+list_garbage_collect (void)
+{
+	struct list_info *new_db;
+
+	assert (module_ok ());
+
+	if (db_size == db_used)
+		return;
+
+	if (db_used == 0) {
+		free (db);
+		db = NULL;
+		db_size = 0;
+	} else {
+		new_db = realloc (db, db_used * sizeof(*db));
+		if (new_db != NULL) {
+			db = new_db;
+			db_size = db_used;
+		}
 	}
 }
 
 
 bool
-list_is_empty (list_node_t *tp)
+list_is_valid (list_t ls)
 {
-	return (tp == NULL);
-}
+	assert (module_ok ());
 
-
-list_node_t *
-list_next (list_node_t *node)
-{
-	return node->n_next;
-}
-
-
-list_node_t *
-list_head (list_node_t *tp)
-{
-	list_node_t *head = NULL;
-	if (!list_is_empty (tp))
-		head = list_next (tp);
-	return head;
+	if (ls != LIST_ERR
+	    && (ls >= 0 || ls < db_used))
+		return TRUE;
+	return FALSE;
 }
 
 
 bool
-list_node_is_last (list_node_t *tp, list_node_t *ptr)
+list_is_empty (list_t ls)
 {
-	return (tp == ptr
-	        && ptr->n_next == ptr
-	        && ptr->n_prev == ptr);
+	assert (module_ok ());
+	assert (list_is_valid (ls));
+
+	if (db[ls].li_tail_ptr == NULL)
+		return TRUE;
+	return FALSE;
+}
+
+
+void *
+list_peek (list_t ls)
+{
+	assert (module_ok ());
+	assert (list_is_valid (ls));
+
+	if (!list_is_empty (ls))
+		return db[ls].li_tail_ptr->n_next;
+	return NULL;
+}
+
+
+bool
+list_length (list_t ls)
+{
+	assert (module_ok ());
+	assert (list_is_valid (ls));
+
+	return db[ls].li_list_len;
 }
 
 
@@ -160,6 +252,7 @@ list_enqueue (list_node_t **tp, list_node_t *new_tail)
 
 list_node_t *
 list_dequeue (list_node_t **tp)
+/* FIXME deve deallocare il list_node_t e ritornare il n_ptr */
 {
 	list_node_t *head = list_head (*tp);
 	if (head != NULL)
